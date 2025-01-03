5d205a86b562550e70208d5f4335da32e9dfb4c3
Nickname <1013238728@qq.com>
2025-01-04 03:15:45 +0800

    for 循环 中使用 copy.deepcopy(product_data), 否则product_list中数据全是一样的

--- 123.py
+++
@@ -1,3 +0,0 @@
-data_all = [13, 19, 23, 50, 64]
-data_all_str = ','.join(map(str, data_all))
-print(data_all_str)
--- test.py
+++ test.py
@@ -3,6 +3,7 @@ import json
 import os
 import csv
 import re
+import copy
 
 '''
     1.会员卡问题
@@ -186,7 +187,7 @@ def getCustomerData(customer,user_data,pet_data,card_data):
 
     # 组装数据到 user_data 中
     customerInfoData = json.loads(customerInfoResponse2.text)['Data']
-    user_data_copy = user_data
+    user_data_copy = copy.deepcopy(user_data)  # 深拷贝
     # task_id 老子不知道
     user_data_copy['owner_id'] = customerInfoData['id'] # 客户ID
     user_data_copy['owner_name'] = customerInfoData['name'] # 客户名称
@@ -231,7 +232,7 @@ def getCustomerData(customer,user_data,pet_data,card_data):
     # 组装数据到 card_data 中
     cardInfoDataList = json.loads(customerInfoResponse1.text)['Data']
     for cardInfoData in cardInfoDataList:
-        card_data_copy = card_data
+        card_data_copy = copy.deepcopy(card_data)  # 深拷贝
         # task_id 老子不知道
         card_data_copy['owner_id'] = customer['id'] # 客户ID
         card_data_copy['card_name'] = cardInfoData['con_card_type_name'] # 折扣卡名称
@@ -275,7 +276,7 @@ def getCustomerData(customer,user_data,pet_data,card_data):
     petInfoDataList = json.loads(customerInfoResponse3.text)['Data']
     for petInfoData in petInfoDataList:
         # 组装数据到 pet_data 中
-        pet_data_copy = pet_data
+        pet_data_copy = copy.deepcopy(pet_data)  # 深拷贝
         # task_id 老子不知道
         pet_data_copy['owner_id'] = customer['id'] # 客户ID
         pet_data_copy['pet_id'] = petInfoData['id'] # 宠物ID
@@ -382,12 +383,43 @@ def getExpenseCalendarData(customer):
         
 # 获取商品信息  
 def getProductData(product_data):
+    # 类目信息
+    catalog_dict = {}
+    # (GET) http://127.0.0.1:13301/base%2fsetting%2fusage
+    product_detail_other1 = headURL + 'base%2fsetting%2fusage'
+    # (GET) http://127.0.0.1:13301/base%2fsetting%2fuseunit
+    product_detail_other2 = headURL + 'base%2fsetting%2fuseunit'
+    # (GET) http://127.0.0.1:13301/base%2fsetting%2fkind
+    product_detail_other3 = headURL + 'base%2fsetting%2fkind'
+    product_detail_other1_response = requests.get(product_detail_other1,headers=headers)
+    product_detail_other2_response = requests.get(product_detail_other2,headers=headers)
+    product_detail_other3_response = requests.get(product_detail_other3,headers=headers)
+
+    file_other = '医院数据/商品信息/'
+    if not os.path.exists(file_other):   # 不存在则创建
+        # 创建文件夹
+        os.makedirs(file_other)
+    
+    # 将商品类目写入到 usage.json 中
+    with open(f'./' + file_other + '/usage.json','w',encoding='utf-8') as f:
+        f.write(product_detail_other1_response.text)
+    # 将商品类目写入到 useunit.json 中
+    with open(f'./' + file_other + '/useunit.json','w',encoding='utf-8') as f:
+        f.write(product_detail_other2_response.text)
+    # 将商品类目写入到 kind.json 中
+    with open(f'./' + file_other + '/kind.json','w',encoding='utf-8') as f:
+        f.write(product_detail_other3_response.text)
+    
+
+
+
     # 获取商品类目  （GET）http://127.0.0.1:13301/base%2fsetting%2fcategory%2fsub%2f1  
     # 备注： 从 1-12分别为：挂号，处方，检验，影像，处置，手术，住院，寄养，疫苗，驱虫，美容，商品；非处方为71
     catalog_list = ['占位','挂号','处方','检验','影像','处置','手术','住院','寄养','疫苗','驱虫','美容','商品','非处方']
     URL_product = headURL + 'base%2fsetting%2fcategory%2fsub%2f'
     for i in range(1,14):
-        print('正在采集商品信息：' + catalog_list[i] + '中！！！')
+        product_list = []
+        # print('正在采集商品信息：' + catalog_list[i] + '中！！！')
         if i == 13:
             URL_product_catalog = URL_product + '71'
         else:
@@ -405,10 +437,17 @@ def getProductData(product_data):
             f.write(response.text)
 
         responseListData = json.loads(response.text)['Data']
+
+        if responseListData == []: 
+            continue
+
         data_all = []
         # 获取商品详细信息  （POST）http://127.0.0.1:13301/base%2fsetting%2fgoods%2fget
         URL_product_detail = headURL + 'base%2fsetting%2fgoods%2fget'
         for responseData in responseListData:
+            print('正在采集商品信息：' + catalog_list[i] + '-' + responseData['name'] + '中！！！')
+            # 记录所有的二级目录 id 映射 name
+            catalog_dict[str(responseData['id'])] = responseData['name']
             # 记录所有的商品二级目录信息
             data_all.append(int(responseData['id']))
             data = int(responseData['id'])
@@ -429,6 +468,74 @@ def getProductData(product_data):
         with open(f'./' + file_ + '/product-all.json','w',encoding='utf-8') as f:
             f.write(responseListAll.text)
 
+        print('正在写入:' + catalog_list[i] + '-商品信息到CSV中！！！')
+
+        # 组装数据到 product_list 中
+        for product in json.loads(responseListAll.text)['Data']:
+            # print(product)
+            # print(replace_special_chars(catalog_dict[str(product['con_category_id'])].strip())) # 口服类
+
+            # 更详细信息  （GET) http://127.0.0.1:13301/base%2fsetting%2fgood%2f1693
+            URL_product_detail_other = headURL + 'base%2fsetting%2fgood%2f' + str(product['id'])
+            product_detail_response = requests.get(URL_product_detail_other,headers=headers)
+            file_product_other = file_ + '/' + replace_special_chars(catalog_dict[str(product['con_category_id'])].strip())
+            if not os.path.exists(file_product_other):   # 不存在则创建
+                # 创建文件夹
+                os.makedirs(file_product_other)
+            # 将所有商品写入到 product-[name].json 中
+            with open(f'./' + file_product_other + '/product-' + replace_special_chars(product['name'].strip()) + '.json','w',encoding='utf-8') as f:
+                f.write(product_detail_response.text)
+            
+            productDetailResponseData = json.loads(product_detail_response.text)['Data']
+
+            product_data_copy = copy.deepcopy(product_data)  # 深拷贝，浅拷贝会出问题
+            # task_id 老子不知道
+            product_data_copy['goods_code'] = product['code'] # 商品编号
+            product_data_copy['sale_state'] = product['on_and_off_shelves'] # 商品是否上下架  0-上架 1-下架
+            product_data_copy['name'] = product['name'] # 商品名称
+            # common_name 通用名 参数无
+            # forshort 简称 参数无
+            # print_name 打印名 参数无
+            product_data_copy['spec'] = product['format'] # 规格
+            product_data_copy['barcode'] = product['encode'] # 条码
+            product_data_copy['category1'] = product['top_category_id'] # 一级类别
+            product_data_copy['category2'] = product['con_category_id'] # 二级类别
+            # category3 三级类别 无参数
+            # category4 四级类别 无参数 
+            product_data_copy['cost_price'] = product['buyprice'] # 成本价
+            product_data_copy['sale_price'] = product['saleprice'] # 销售价
+            product_data_copy['unit'] = product['unit'] # 单位
+            product_data_copy['dosageunit'] = productDetailResponseData['useunit'] # 用量单位
+            # allow_addcountcard 允许开卡 无参数
+            product_data_copy['is_calculating_store'] = product['isuse_stock'] # 是否计算库存 0-否 1-是
+            # safe_store 安全库存 无参数
+            product_data_copy['use_method'] = product['usages'] # 给药途径
+            # printlable 打印标签 无参数
+            # drug_message 药品信息 无参数
+            # drug_tips 药品Tips 无参数
+            product_data_copy['manufacturer'] = product['vender'] # 生产厂商
+            product_data_copy['brand_name'] = product['brand'] # 品牌名称
+            # license_number 批准文号 无参数   
+            product_data_copy['remark'] = product['guideline'] # 备注-说明
+            # supplier 供应商 无参数
+            # inspection_name 化验名称 无参数
+            # inspection_brand 化验品牌名 无参数
+            # inspection_model 化验厂商名 无参数
+            # stock 库存 无参数
+
+            product_list.append(product_data_copy)
+
+        csv_file = './医院数据/商品信息.csv'
+        # 检查文件是否存在且不为空
+        file_exists = os.path.isfile(csv_file) and os.path.getsize(csv_file) > 0
+        # 保存到.csv文件中
+        with open(csv_file,'a',encoding='utf-8',newline='') as f:
+            # 将文件对象转换成 DictWriter 对象
+            writer = csv.DictWriter(f,fieldnames = product_data.keys())
+            # 如果文件是新创建的，写入表头
+            if not file_exists:
+                writer.writeheader()
+            writer.writerows(product_list)
         
 
 
